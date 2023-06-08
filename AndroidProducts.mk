16bb261661260528eddd487f6d31b015fdfe430c
Štěpán <stepanvitasek1@gmail.com>
2023-06-08 10:04:36 +0200

    beyond2lte: blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond2lte.mk
+    $(LOCAL_DIR)/bliss_beyond2lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond2lte-eng \
-    lineage_beyond2lte-user \
-    lineage_beyond2lte-userdebug
+    bliss_beyond2lte-eng \
+    bliss_beyond2lte-user \
+    bliss_beyond2lte-userdebug
